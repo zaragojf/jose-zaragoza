fddfb70cb94b059130399a0693714bb2e7acea27
jose-zaragoza <zaragojf@gmatc.matc.edu>
2021-12-08 01:38:58 -0600

    Completed Project 2

--- jose-zaragoza-p2/js/scripts.js
+++ jose-zaragoza-p2/js/scripts.js
@@ -1,110 +1,7 @@
-/**
- * Wrap everything in an IEFE (Immediately Invoked Function Expression) to keep
- * our variables constrained to the scope of this function and out of the global scope.
- */
-(function() {
-
-    /*************************************
-    * Package data and constructor objects
-    *************************************/
-
-    // Package data array (simulated data source, such as JSON or database recordset)
-    var data = [
-        {
-            description: "Men's Monday night softball for Milwaukee AG Churches playing at Wick Field.",
-            selector: 'p1'
-        },
-        {
-            name: 'Dennis Holmstrom',
-            selector: 'p2'
-        },
-        {
-            name: 'Jesus Serna (OC Royals)',
-            selector: 'p3'
-        },
-        {
-            name: 'Jose Zaragoza (Root River Shock)',
-            selector: 'p4'
-        },
-        {
-            name: 'Jose Zaragoza (Root River Shock)',
-            selector: 'p5'
-        }
-    ];
-
-    // (VS Code) Package constructor function
-    function Package(data) {
-        this.name = data.name;
-        this.description = data.description;
-    };
-
-    /*******************
-     * Utility functions
-     ******************/
-
-    // Returns today's date, formatted
-    var getTodaysDate = function() {
-        var today = new Date();
-        return today.toDateString();
-    };
-
-    // Returns DOM element by id.
-    // We're just wrapping document.getElementById
-    // in a shorthand function. If this seems confusing,
-    // then just replace getEl with document.getElementById
-    // in the writePackageInfo function.
-    var getEl = function (id) {
-        return document.getElementById(id);
-    };
-
-    /**
-     * Write's the package object's data to the appropriate
-     * DOM elements utilizing the package selector property.
-     */
-    var writePackageInfo = function (package) {
-        // Get reference to DOM elements
-        var selector = package.selector,
-            nameEl = getEl(selector + '-name'),
-
-            // Write package data to DOM elements
-            nameEl.textContent = package.name;
-    };
-
-    // Write date
-    var dateEl = getEl('date');
-    dateEl.textContent = getTodaysDate();
-
-    // Write package data one-by-one
-    var Descript = new Package(data[0]);
-    writePackageInfo(Descript);
-
-    var Director = new Package(data[1]);
-    writePackageInfo(Director);
-
-    var SportsmanshipCommittee = new Package(data[2]);
-    writePackageInfo(SportsmanshipCommittee);
-
-    var SportsmanshipCommittee2 = new Package(data[3]);
-    writePackageInfo(SportsmanshipCommittee2);
-
-    var RecordKeeping = new Package(data[4]);
-    writePackageInfo(RecordKeeping);
-
-}());
-
-
-
-
-
-
-
-
-
 // Utility functions
 function get(element) {
     return document.getElementById(element);
 }
-
 
 // Application functions
 function openModal() {
@@ -161,5 +58,88 @@ window.addEventListener('load', function () {
     saveButton.addEventListener('click', saveContent);
 });
 
+/**
+ * Wrap everything in an IEFE (Immediately Invoked Function Expression) to keep
+ * our variables constrained to the scope of this function and out of the global scope.
+ */
+(function() {
 
+    /*************************************
+    * Package data and constructor objects
+    *************************************/
 
+    // Package data array (simulated data source, such as JSON or database recordset)
+    var data = [
+        {
+            name: 'Mens Monday night softball for Milwaukee AG Churches playing at Wick Field.',
+            selector: 'p1'
+        },
+        {
+            name: 'Dennis Holmstrom',
+            selector: 'p2'
+        },
+        {
+            name: 'Jesus Serna',
+            selector: 'p3'
+        },
+        {
+            name: 'Jose Zaragoza',
+            selector: 'p4'
+        }
+    ];
+
+    // (VS Code) Package constructor function
+    function Package(data) {
+        this.name = data.name;
+    };
+
+    /*******************
+     * Utility functions
+     ******************/
+
+    // Returns today's date, formatted
+    var getTodaysDate = function() {
+        var today = new Date();
+        return today.toDateString();
+    };
+
+    // Returns DOM element by id.
+    // We're just wrapping document.getElementById
+    // in a shorthand function. If this seems confusing,
+    // then just replace getEl with document.getElementById
+    // in the writePackageInfo function.
+    var getEl = function (id) {
+        return document.getElementById(id);
+    };
+
+    /**
+     * Write's the package object's data to the appropriate
+     * DOM elements utilizing the package selector property.
+     */
+    var writePackageInfo = function (package) {
+        // Get reference to DOM elements
+        var selector = package.selector,
+            nameEl = getEl(selector + '-name');
+
+            // Write package data to DOM elements
+            nameEl.textContent = package.name;
+    };
+
+    // Write date
+    var dateEl = getEl('date');
+    dateEl.textContent = getTodaysDate();
+
+    // Write package data one-by-one
+    var Descript = new Package(data[0]);
+    writePackageInfo(Descript);
+
+    var Director = new Package(data[1]);
+    writePackageInfo(Director);
+
+    var SportsmanshipCommittee = new Package(data[2]);
+    writePackageInfo(SportsmanshipCommittee);
+
+    var RecordKeeping = new Package(data[3]);
+    writePackageInfo(RecordKeeping);
+
+}());
